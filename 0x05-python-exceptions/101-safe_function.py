eeb632dc107bdfa3e046804eb8167aed52fc3a6f
phirstchild <117740003+phirstchild@users.noreply.github.com>
2023-03-27 20:45:34 +0100

    adv task 1

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 import sys
 
 
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
+    return result
