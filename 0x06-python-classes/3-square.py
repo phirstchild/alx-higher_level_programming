68f7cadc7655e3cc2bbc5d267e55c041c2d77ace
phirstchild <117740003+phirstchild@users.noreply.github.com>
2023-03-30 10:43:58 +0100

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,8 +8,8 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new square.
-
         Args:
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -17,6 +17,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """Return the current area of the square."""
-            return (self.__size * self.__size)
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
