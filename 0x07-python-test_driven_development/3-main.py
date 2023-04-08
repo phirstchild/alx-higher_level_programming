5cbe21dd08ae8f76c19257d8e7b13f6b27fefa48
phirstchild <117740003+phirstchild@users.noreply.github.com>
2023-04-08 02:19:31 +0100

    task 2

--- 0x07-python-test_driven_development/3-main.py
+++ 0x07-python-test_driven_development/3-main.py
@@ -8,10 +8,6 @@ try:
     say_my_name(12, "White")
 except Exception as e:
     print(e)
-<<<<<<< HEAD
-=======
-    
->>>>>>> ef26704ac60885b2335ad0c272c8d8f4d0db1815
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
