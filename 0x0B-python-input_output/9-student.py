8e5135ba2285eda94c1859bbf69f430c11431d03
Zainab <sulaiman.zainab79@gmail.com>
2023-04-12 13:53:42 -0700

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define a class Student."""
+
+
+class Student:
+    """Represent the class student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): first name of the student.
+            last_name (str): last name of the student.
+            age (int):  age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
