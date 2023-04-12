b1a58f19578ca58bb7683d8eca906451b94a005f
phirstchild <117740003+phirstchild@users.noreply.github.com>
2023-04-12 22:42:13 +0100

    task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-"""A module that reads from standard input and computes metrics.
+"""
+Read from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
@@ -9,14 +9,17 @@ prints the following statistics:
 
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics.
+    """
+    Print accumulated metrics.
+
     Args:
-        size (int):  accumulated read file size.
-        status_codes (dict): accumulated count of status codes.
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
